1b2ed6ff4c2639ad9ec5da133a5bebdc28ad8751
julienrull <julien.rull38@gmail.com>
2024-08-02 18:41:35 +0200

    [IMP] working arena allocator

--- arena.h
+++ arena.h
@@ -5,9 +5,12 @@
     // * Definitions only
     typedef struct Arena *Arena; 
     Arena arena_make(size_t size);
-    void* arena_alloc(Arena arena, size_t size);
+    void* arena_alloc(Arena arena, size_t size, size_t alignment);
     void arena_free(Arena arena);
 #else
+    #ifndef __ALIGNMENT__
+        #define __ALIGNMENT__ (2 * sizeof(void *))
+    #endif
     // * Implementations
     typedef struct Arena{
         void* base;
@@ -16,6 +19,9 @@
         size_t committed;
     }*Arena;
     Arena arena_make(size_t size){
+        if(!size){
+            return 0;
+        }
         Arena arena = malloc(sizeof(struct Arena));
         arena->size = size;
         arena->offset = 0;
@@ -23,11 +29,19 @@
         arena->base = malloc(size);
         return arena;
     }
-    void* arena_alloc(Arena arena, size_t size){
-        void* mem = arena->base + arena->offset; 
+    void* arena_alloc(Arena arena, size_t size, size_t alignment){
+        void* curr_ptr = arena->base + arena->offset; 
+        size_t len = curr_ptr - arena->base;
+        size_t modulo =  (len % alignment);
+        if(len + size > arena->size) {
+            return 0;
+        }
+        if(modulo) {
+           curr_ptr += alignment - modulo; 
+        }
         arena->offset += size;
         arena->committed += size;
-        return mem;
+        return curr_ptr;
     }
     void arena_free(Arena arena) {
        free(arena->base); 
--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #define __ARENA_IMPL__
 #include"arena.h"
-
+#include <stdint.h>
 
 int main() {
     Arena arena = arena_make(1024);
-    int* val = arena_alloc(arena, sizeof(int));
-    *val = 666;
-    int* val2 = arena_alloc(arena, sizeof(int));
-    *val2 = 555;
-    printf("val: %d, val2: %d\n", *val, *val2);
-    arena_free(arena);
+    int* val = arena_alloc(arena, sizeof(int), __ALIGNMENT__);
+    size_t i = 1;
+    while(val != 0) {
+        val = arena_alloc(arena, sizeof(int), __ALIGNMENT__);
+        i++;
+        printf("%lu - %lu\n", i, arena->committed);
+    }
+    printf("DONE\n");
     return 0;
 }
