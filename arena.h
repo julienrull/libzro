8274ec554c9859e800baad3f5398dd9dbfd7c1b5
Julien Rull <julienrull@MacBook-Air-de-Julien.local>
2024-08-07 16:10:44 +0200

    [IMP] Arena unit tests

--- arena.h
+++ arena.h
@@ -3,6 +3,7 @@
     #define __ARENA__
     #include<stdio.h>
     #include<stdlib.h>
+    #define __ALLOCATOR_IMPL__
     #include "allocator.h"
     // *** ALLOCATORS ***  
     #ifndef __ALIGNMENT__
@@ -13,51 +14,80 @@
     #ifndef __ARENA_IMPL__
             // *** ALLOCATORS ***  
             typedef struct Arena; 
+            uintptr_t align_forward(uintptr_t ptr, size_t alignment)
             void *arena_alloc_aligned(Arena *arena, size_t size, size_t alignment);
             void* arena_alloc(Arena arena, size_t size, size_t alignment);
             void arena_free(size_t size, void *ptr, void *context);
             void arena_free_all(void *context);
             Arena arena_init(void *buffer, size_t size);
     #else
+            #define DEFAULT_ALIGNMENT (2 * sizeof(void *))
+
             typedef struct {
-                void* base;
+                void *base;
                 size_t size;
                 size_t offset;
                 size_t committed;
             } Arena;
-            void *arena_alloc_aligned(Arena *arena, size_t size, size_t alignment){
-                void *curr_ptr = arena->base + arena->offset; 
-                size_t len = curr_ptr - arena->base;
-                size_t modulo =  (len % alignment);
-                if(!is_power_of_two(alignment)){
+            
+            #define arena_alloc_init(a) (Allocator){arena_alloc, arena_free, a}
+            
+            #define is_power_of_two(x) ((x != 0) && ((x & (x - 1)) == 0))
+            
+            uintptr_t align_forward(uintptr_t ptr, size_t alignment) {
+                uintptr_t p, a, modulo;
+                if (!is_power_of_two(alignment)) {
                     return 0;
                 }
-                if(len + size > arena->size) {
-                    return 0;
+            
+                p = ptr;
+                a = (uintptr_t)alignment;
+                modulo = p & (a - 1);
+            
+                if (modulo) {
+                    p += a - modulo;
                 }
-                if(modulo) {
-                   curr_ptr += alignment - modulo; 
-                }
-                arena->offset += size;
-                arena->committed += size;
-                return curr_ptr;
+            
+                return p;
             }
-            void *arena_alloc(size_t size, void *context){
-                if(!size) {
-                 return 0;
-                } 
-                return arena_alloc_aligned((Arena *)context, size, __DEFAULT_ALIGNMENT__);
-            } 
+            
+            void *arena_alloc_aligned(Arena *a, size_t size, size_t alignment) {
+                uintptr_t curr_ptr = (uintptr_t)a->base + (uintptr_t)a->offset;
+                uintptr_t offset = align_forward(curr_ptr, alignment);
+                offset -= (uintptr_t)a->base;
+            		
+                if (offset + size > a->size) {
+                    return 0;
+                }
+            
+                a->committed += size;
+                void *ptr = (uint8_t *)a->base + offset;
+                a->offset = offset + size;
+            
+                return ptr;
+            }
+            
+            void *arena_alloc(size_t size, void *context) {
+                if (!size) {
+                    return 0;
+                }
+                return arena_alloc_aligned((Arena *)context, size, DEFAULT_ALIGNMENT);
+            }
+            
+            // Does nothing.
             void arena_free(size_t size, void *ptr, void *context) {
                 (void)ptr; (void)size; (void)context;
             }
+            
             void arena_free_all(void *context) {
                 Arena *a = context;
                 a->offset = 0;
                 a->committed = 0;
             }
+            
             Arena arena_init(void *buffer, size_t size) {
                 return (Arena){.base = buffer, .size = size};
             }
+
     #endif
 #endif
---
+++ tests/arana.test.c
@@ -0,0 +1,68 @@
+#define __ARENA_IMPL__
+#include "../tests.h"
+#include "../arena.h"
+
+int tests_run = 0;
+
+static char * test_arena_init() {
+    unsigned char *buffer = malloc(1024);
+    Arena arena = arena_init(buffer, 1024);
+    Allocator allocator = arena_alloc_init(&arena);
+    mu_assert("error, &arena != allocator.context", &arena == allocator.context);
+    mu_assert("error, buffer != ((Arena*)(allocator.context))->base", buffer == ((Arena*)(allocator.context))->base);
+    mu_assert("error, ((Arena*)(allocator.context))->size != 1024", ((Arena*)(allocator.context))->size == 1024);
+    mu_assert("error, ((Arena*)(allocator.context))->offset != 0", ((Arena*)(allocator.context))->offset == 0);
+    mu_assert("error, ((Arena*)(allocator.committed))-> != 0", ((Arena*)(allocator.context))->committed == 0);
+    return 0;
+}
+
+static char * test_arena_alloc() {
+    unsigned char *buffer = malloc(1024);
+    Arena arena = arena_init(buffer, 1024);
+    Allocator allocator = arena_alloc_init(&arena);
+    int *val = allocator.alloc(sizeof(int), allocator.context);
+    *val = 555;
+    mu_assert("error, val != (int*)arena.base", val == (int*)arena.base);
+    mu_assert("error, arena.offset != sizeof(int)", arena.offset == sizeof(int));
+    mu_assert("error, arena.committed != sizeof(int)", arena.committed == sizeof(int));
+    mu_assert("error, *val != 555", *val == 555);
+    int *val2 = allocator.alloc(sizeof(int), allocator.context);
+    *val2 = 444;
+    mu_assert("error, val2 != (int*)(arena.base + 16)", val2 == (int*)(arena.base + 16));
+    printf("%lu\n", arena.offset);
+    mu_assert("error, arena.offset != 16 + sizeof(int)", arena.offset == 16 + sizeof(int));
+    mu_assert("error, arena.committed != sizeof(int) * 2", arena.committed == sizeof(int) * 2);
+    mu_assert("error, *val2 != 444", *val2 == 444);
+    return 0;
+}
+
+static char * test_arena_free_all() {
+    unsigned char *buffer = malloc(1024);
+    Arena arena = arena_init(buffer, 1024);
+    Allocator allocator = arena_alloc_init(&arena);
+    int *val = allocator.alloc(sizeof(int), allocator.context);
+    int *val2 = allocator.alloc(sizeof(int), allocator.context);
+    arena_free_all(allocator.context);
+    mu_assert("error, arena.offset != 0", arena.offset == 0);
+    mu_assert("error, arena.committed != 0", arena.committed == 0);
+    return 0;
+}
+
+static char * all_tests() {
+    mu_run_test(test_arena_init);
+    mu_run_test(test_arena_alloc);
+    mu_run_test(test_arena_free_all);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+    return result != 0;
+}
